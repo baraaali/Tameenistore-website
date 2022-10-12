850f76b800dbde380981ec7d8bc4b7882ac74cd6
hanasimoh <hanasimoh@gmail.com>
2022-10-12 23:00:33 +0100

    edit the plan controller

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,14 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-    Route::get('/', function () {
-        return view('welcome');
-    });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-    require __DIR__.'/auth.php';
+require __DIR__.'/auth.php';
 
-
+Route::resousce('plan', 'PlanController');
