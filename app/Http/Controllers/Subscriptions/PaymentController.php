843617ac24edd7a5dde13071335b7756371f9e4a
hanasimoh <hanasimoh@gmail.com>
2022-10-22 23:18:51 +0100

    add validation to subscription controller

--- app/Http/Controllers/Subscriptions/PaymentController.php
+++ app/Http/Controllers/Subscriptions/PaymentController.php
@@ -7,5 +7,26 @@ use Illuminate\Http\Request;
 
 class PaymentController extends Controller
 {
-    //
+    public function index() {
+        $data = [
+            'intent' => auth()->user()->createSetupIntent()
+        ];
+
+        return view('subscriptions.payment')->with($data);
+    }
+
+    public function store(Request $request) {
+        
+        $this->validate($request, [
+            'token' => 'required'
+        ]);
+
+        $plan = Plans::where('identifier', $request->plan)
+            ->orWhere('identifier', 'basic')
+            ->first();
+        
+        $request->user()->newSubscription('default', $plan->stripe_id)->create($request->token);
+
+        return back();
+    }
 }
